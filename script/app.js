1c95e482e08234cfcc0d2da37f6349e5434d7299
Evenchen21 <guy.1248@gmail.com>
2025-03-23 20:21:48 +0200

    js

--- script/app.js
+++ script/app.js
@@ -1,1 +1,6 @@
-submitButtonForm
+const submitButtonForm = document.querySelector(".submitButtonForm");
+
+submitButtonForm.addEventListener("click", function () {
+  alert("Thank you , we will contact you soon");
+  window.location.href = "index.html";
+});
